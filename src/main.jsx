5d75144d9f8d4e310b0acf810abdc393d1cd9e5f
IzcoGLeon <Izco.g.leon@outlook.com>
2024-01-11 14:43:15 -0600

    set deploy for ghpages following routing adaptations

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -11,7 +11,7 @@ import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    < BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
